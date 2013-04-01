9b23c462f4d9be80bca05f858167ee4c7c9159cf
Delos Chang <delosfchang@gmail.com>
2013-04-01 18:39:58 -0400

    updating for heroku

--- auto_class_scheduler/settings.py
+++ auto_class_scheduler/settings.py
@@ -225,3 +225,10 @@ TEMPLATE_CONTEXT_PROCESSORS += (
     'django.core.context_processors.request',
 )
 
+# heroku
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
