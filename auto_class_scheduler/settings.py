052166f21ed1e1736768574c96a1626e9cdf2674
Delos Chang <delosfchang@gmail.com>
2013-04-01 19:32:29 -0400

    updating settings

--- auto_class_scheduler/settings.py
+++ auto_class_scheduler/settings.py
@@ -64,13 +64,12 @@ STATIC_ROOT = ''
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-if node() 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.environ['AUTO_CLASS_STATIC']
+    os.environ['AUTO_CLASS_STATIC'],
 )
 
 # List of finder classes that know how to find static files in
@@ -110,7 +109,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.environ['AUTO_CLASS_TEMPLATE']
+    os.environ['AUTO_CLASS_TEMPLATE'],
 )
 
 INSTALLED_APPS = (
