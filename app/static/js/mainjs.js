c5bf6c16b2d1299b050f805838012d56b34a7085
Delos Chang <delosfchang@gmail.com>
2013-04-01 16:35:57 -0400

    sorted image complications

--- app/static/js/mainjs.js
+++ app/static/js/mainjs.js
@@ -1,10 +1,5 @@
   // Tutorial JS 
   $('#start_me').live('click', function(){
-    try{
-      mixpanel.track("Clicked LetsStart button tutorial");
-    } catch(e) {
-      console.log('start');
-    }
     $('#top_instruction').fadeOut('medium', function(){
       $('#lower_instruction').html('Name a class you\'re taking').attr('class', 'lead');
       $('#start_me').hide();
