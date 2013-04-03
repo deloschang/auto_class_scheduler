3c5bc811ec3e3af27d88d85081f0558b634af86c
Delos Chang <delosfchang@gmail.com>
2013-04-03 16:08:05 -0400

    reenabling full features

--- app/views.py
+++ app/views.py
@@ -273,8 +273,8 @@ def insert_to_calendar(user, class_name, period):
       ],
     }
 
-    #created_event = service.events().insert(calendarId='primary', body=event).execute()
-    #print "Created Event: %s" % created_event['id']
+    created_event = service.events().insert(calendarId='primary', body=event).execute()
+    print "Created Event: %s" % created_event['id']
 
 def check_period_time(period):
     # maybe parse from the link in the future?
