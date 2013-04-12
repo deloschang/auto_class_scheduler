78c25c2ecf1ce4d3288e5352d48e5f5f21d13e02
Delos Chang <delosfchang@gmail.com>
2013-04-11 23:13:31 -0400

    javascript attachment for the error handling

--- app/static/js/mainjs.js
+++ app/static/js/mainjs.js
@@ -1,6 +1,7 @@
   // Tutorial JS 
   $('#start_me').live('click', function(){
     $('#top_instruction').fadeOut('medium', function(){
+      $('#error_message').hide();
       $('#lower_instruction').html('Name a class you\'re taking').attr('class', 'lead');
       $('#start_me').hide();
       $('#friend_div').fadeIn('slow');
